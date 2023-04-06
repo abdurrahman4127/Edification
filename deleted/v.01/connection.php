4dcf841c594c24def5da36015fd6831cf084b53e
root <root@localhost.localdomain>
2023-04-06 16:20:24 +0600

    previous version deleted

--- deleted/v.01/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $con = mysqli_connect("localhost", "root", "", "edification");
-
-    if (mysqli_connect_errno()){
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    }
-?>
--- deleted/v.01/logout.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    session_start();
-    
-    // terminating session
-    session_unset();
-    session_destroy();
-    
-    header("Location: index.php");
-?>
